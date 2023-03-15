c22bee01decfbbf87f6fab1528c7b9c0d1b404e3
RodoJML <rodojml@me.com>
2023-03-15 19:33:43 -0400

    deleting unnecesary files, navbar update

---
+++ client/src/components/NavBar.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <nav class="navbar is-warning">
+        <div class="container">
+            <div class="navbar-brand">
+                <a class="navbar-item" href="#">
+                    <img src="@/assets/6626465.png" alt="foodLogo" width="112" height="28">
+                </a>
+                
+            </div>
+        </div>
+
+    </nav>
+</template>
+
+<style scoped>
+
+</style>
--- client/src/router/index.ts
+++ client/src/router/index.ts
@@ -4,13 +4,12 @@ import HomeView from '../views/HomeView.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    {path: '/',       name: 'home',   component: HomeView},
+    {path: '/', name: 'home', component: HomeView},
     //{path: '/about',  name: 'about',  component: () => import('../views/AboutView.vue')}
   ]
 })
 
 export default router
-
 
 function secureRoute(to: RouteLocationNormalized, from: RouteLocationNormalized, next: NavigationGuardNext){
   // Here we will be creating the secure routes
