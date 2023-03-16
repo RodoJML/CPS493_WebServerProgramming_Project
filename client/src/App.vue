3d436ecf28ce839eaa561799789600d19fd914fa
RodoJML <rodojml@me.com>
2023-03-15 21:41:01 -0400

    adding images and dummy data

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,16 +1,25 @@
 <script setup lang="ts">
 import HomeView from './views/HomeView.vue';
+import NavBar from './components/NavBar.vue';
+
 </script>
 
 <template>
   <header>
-    <div class="wrapper">
+
+    <NavBar/>
+
+    <div class="container">
       <HomeView/>
     </div>
+    
   </header>
 
 </template>
 
 <style scoped>
+  header {
+    background-color: #FEB84B;
+  }
 
 </style>
--- client/src/components/NavBar.vue
+++ client/src/components/NavBar.vue
@@ -1,21 +1,52 @@
 <script setup lang="ts">
-
+    import { RouterLink } from 'vue-router';
 </script>
 
 <template>
     <nav class="navbar is-warning">
         <div class="container">
             <div class="navbar-brand">
-                <a class="navbar-item" href="#">
-                    <img src="@/assets/6626465.png" alt="foodLogo" width="112" height="28">
-                </a>
-                
+
+                <RouterLink to="/" class="navbar-item">
+                    <img src="@/assets/fnd.png" alt="foodLogo" width="28" height="28">
+                    <div class="title">Cal<span class="titleC">C</span></div>
+                </RouterLink>
+
+                <div class="navbar-burger">
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </div>
+            </div>
+
+            <div class="navbar-menu">
+                <div class="navbar-start">
+                    <RouterLink to="/dashboard" class="navbar-item">Dashboard</RouterLink>
+                    <RouterLink to="/food" class="navbar-item">Food</RouterLink>
+                </div>
+
+                <div class="navbar-end">
+                    <RouterLink to="/contact" class="navbar-item">Contact</RouterLink>
+                    <RouterLink to="/login" class="navbar-item">Login</RouterLink>
+                </div>
+
             </div>
         </div>
+
 
     </nav>
 </template>
 
 <style scoped>
+.title {
+    color: sienna;
+}
 
+.titleC {
+    color: darkslategray;
+}
+
+.navbar-item {
+    color: sienna;
+}
 </style>
