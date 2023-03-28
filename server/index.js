048beecc2a2df978d5d56b2bd4eabee41d2df9c8
RodoJML <rodojml@me.com>
2023-03-27 22:04:02 -0400

    API implementing, pending...

--- server/index.js
+++ server/index.js
@@ -1,30 +1,13 @@
-const http = require('http');
-// Contains all exports from http 
-// Any function from HTTP is now available here
+const express = require('express');
+const path = require('path');
 
+const food = require('./data/food.json');
 
 const hostname = '127.0.0.1';                       
-// Loopbackl address
 const port = process.env.PORT || 3000;              
-// Commonly used as a development port
 
-const server = http.createServer((req, res) => {
-// When a text message comes in, run this function...
+const app = express();
 
-    // Remember Http is plain text
-    // Server translate text to what client is asking for
-
-    // res is an object to send messages to the client
-
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('My Final Project for Web Programming / Started March 6, 2023');
-});
-
-
-server.listen(port, () => {
-// Listens for network traffic, this is a call for the Operating System.
-// Node code runs **once** and makes a server in memory 
-
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+app.use(express.json());
+app.use(express.static(path.join(__dirname, '../client/dist')));
+app.listen(port, () => console.log(`Server running at http://${hostname}:${port}/`));
