3c708bf07b9a8c042640a640bd9dbda5bf148c9c
RodoJML <rodojml@me.com>
2023-04-30 17:03:35 -0400

    making adjustments to implement auth with client

--- client/src/model/users.ts
+++ client/src/model/users.ts
@@ -2,6 +2,16 @@ import { api } from './session';
 import type { User } from './session';
 import type { DataEnvelope, DataEnvelopeList } from './myFetch';
 
+export interface User{
+    id?: number;
+    name?: string;
+    email?: string;
+    user?: string;
+    password?: string;
+    photo?: string;
+    token?: string;
+}
+
 export function getUsers(): Promise<DataEnvelopeList<User>> {
     return api('/users');
 }
