817f53023d96454377a325671bb93de5f985745e
RodoJML <rodojml@me.com>
2023-05-01 17:54:29 -0400

    adding require images

--- client/src/App.vue
+++ client/src/App.vue
@@ -2,6 +2,19 @@
 import NavBar from './components/NavBar.vue';
 import { RouterView } from 'vue-router';
 
+require("@/assets/Donut.png");
+require("@/assets/Drumstick.png");
+require("@/assets/eating.png");
+require("@/assets/fnd.png");
+require("@/assets/Fries.png");
+require("@/assets/Glass_Drink.png");
+require("@/assets/Hot_Dog.png");
+require("@/assets/kcal.png");
+require("@/assets/Sandwich.png");
+require("@/assets/settings.png");
+require("@/assets/settings2.png");
+require("@/assets/Soda_Can.png");
+require("@/assets/Soda_Cup.png");
 </script>
 
 <template>
@@ -13,9 +26,9 @@ import { RouterView } from 'vue-router';
   </header>
 </template>
 
-
 <style scoped>
 header {
   background-color: #FEB84B;
 }
+
 </style>
