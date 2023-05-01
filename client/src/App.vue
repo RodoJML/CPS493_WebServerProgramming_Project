e7a0d530488f889d0d58337ef686b19c1d507d14
RodoJML <rodojml@me.com>
2023-05-01 18:14:03 -0400

    pkg jason update to add assets

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,21 +1,6 @@
 <script setup lang="ts">
 import NavBar from './components/NavBar.vue';
 import { RouterView } from 'vue-router';
-
-
-require(src="@/assets/Donut.png");
-require("@/assets/Drumstick.png");
-require("@/assets/eating.png");
-require("@/assets/fnd.png");
-require("@/assets/Fries.png");
-require("@/assets/Glass_Drink.png");
-require("@/assets/Hot_Dog.png");
-require("@/assets/kcal.png");
-require("@/assets/Sandwich.png");
-require("@/assets/settings.png");
-require("@/assets/settings2.png");
-require("@/assets/Soda_Can.png");
-require("@/assets/Soda_Cup.png");
 </script>
 
 <template>
